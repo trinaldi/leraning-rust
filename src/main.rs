fdb321c406f1d902ddef09eb314fd4bb430eb66e
Tiago Rinaldi <trinaldi@gmail.com>
2020-09-01 02:49:48 -0300

    Add shadowing understanding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 10;
+    println!("x equals {}",x);
+
+    // We can't change this variable because it is imutable, right?
+    // well, yes. We can use shadowing; declaring the same variable
+    // using `let` and - of course - the same variable name.
+
+    let x = "Tiago";
+    println!("x equals {}", x);
+
+    // seems like cheating, right?
+    // for what I understand using `let` again will create the variable again
+    // thus allowing us to 'change' it. If you think about it, it's not change
+    // it's recreating. Besides, imutable doesn't necessary means const.
 }
